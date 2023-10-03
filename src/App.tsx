5e668fc87f1e3ee5363ad36b0519f0779eba9903
Manuel Ojeda <manuel.ojeda@washpost.com>
2023-10-03 14:44:48 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from "react";
+import { DateTime } from 'luxon'
+import "./index.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import moment from "moment-timezone";
+
+export function App() {
+
+  const defaultList = ["A", "B", "C", "D", "E"];
+  // React state to track order of items
+  const [itemList, setItemList] = useState(defaultList);
+
+  // Function to update list on drop
+  const handleDrop = (droppedItem: any) => {
+    // Ignore drop outside droppable container
+    if (!droppedItem.destination) return;
+    const updatedList = [...itemList];
+    // Remove dragged item
+    const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
+    // Add dropped item
+    updatedList.splice(droppedItem.destination.index, 0, reorderedItem);
+    // Update State
+    setItemList(updatedList);
+  };
+
+  useEffect(() => {
+    const date = new Date()
+    const luxonDate = DateTime.fromJSDate(date)
+    const momentDate = moment(date)
+
+    console.log(luxonDate.toString(), momentDate.toString(), date)
+  }, [])
+
+  return (
+    <div className="App">
+      <DragDropContext onDragEnd={handleDrop}>
+        <Droppable droppableId="list-container">
+          {(provided) => (
+            <div
+              className="list-container"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {itemList.map((item, index) => (
+                <Draggable key={item} draggableId={item} index={index}>
+                  {(provided) => (
+                    <div
+                      className="item-container"
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                    >
+                      {item}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
